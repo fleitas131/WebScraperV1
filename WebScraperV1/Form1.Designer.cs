6cc5d5e2f7796fcd9c167b3ba5abbe7480c3e34a
arturo <arturofleitasc@gmail.com>
2023-01-28 20:50:00 -0300

    commit inicial

---
+++ WebScraperV1/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace WebScraperV1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gvAmzList = new System.Windows.Forms.DataGridView();
+            this.Posicion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Titulo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btVer = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gvAmzList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gvAmzList
+            // 
+            this.gvAmzList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvAmzList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Posicion,
+            this.Titulo});
+            this.gvAmzList.Location = new System.Drawing.Point(12, 116);
+            this.gvAmzList.Name = "gvAmzList";
+            this.gvAmzList.ReadOnly = true;
+            this.gvAmzList.Size = new System.Drawing.Size(373, 165);
+            this.gvAmzList.TabIndex = 0;
+            // 
+            // Posicion
+            // 
+            this.Posicion.HeaderText = "Posicion";
+            this.Posicion.Name = "Posicion";
+            // 
+            // Titulo
+            // 
+            this.Titulo.HeaderText = "Titulo";
+            this.Titulo.Name = "Titulo";
+            // 
+            // btVer
+            // 
+            this.btVer.Location = new System.Drawing.Point(150, 72);
+            this.btVer.Name = "btVer";
+            this.btVer.Size = new System.Drawing.Size(75, 23);
+            this.btVer.TabIndex = 2;
+            this.btVer.Text = "Ver";
+            this.btVer.UseVisualStyleBackColor = true;
+            this.btVer.Click += new System.EventHandler(this.btVer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 293);
+            this.Controls.Add(this.btVer);
+            this.Controls.Add(this.gvAmzList);
+            this.Name = "Form1";
+            this.Text = "Top 50 PY Spotify";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gvAmzList)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gvAmzList;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Posicion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Titulo;
+        private System.Windows.Forms.Button btVer;
+    }
+}
+
---
+++ WebScraperV1/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+namespace WebScraperV1
+{
+    public partial class Form1 : Form
+    {
+
+        public string enlace = "https://kworb.net/spotify/country/py_daily.html";
+        public string node = "//tr";
+        public string nodeNombre = "//tr/td[@class='text mp']";
+        public int i = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        void cargarItems()
+        {
+            var articulos = new List<articulo>();
+
+            HtmlWeb web = new HtmlWeb();
+            HtmlAgilityPack.HtmlDocument doc = web.Load(enlace);
+
+            var nodes = doc.DocumentNode.SelectNodes(node);
+            
+            foreach (var node in nodes)
+            {
+                i++;
+
+                try
+                {
+                    var articulo = new articulo
+                    {
+                        posicion = i.ToString(),
+                        titulo = node.SelectSingleNode(nodeNombre).InnerText
+                    };
+
+                    articulos.Add(articulo);
+                }
+                catch
+                {
+
+                }
+
+                if (i >= 50)
+                {
+                    break;
+                }
+            }
+            
+
+            foreach (var articulo in articulos)
+            {
+                gvAmzList.Rows.Add(articulo.posicion, articulo.titulo);
+            }
+        }
+
+        class articulo
+        {
+            public string posicion { get; set; }
+            public string titulo { get; set; }
+        }
+
+        private void btVer_Click(object sender, EventArgs e)
+        {
+            cargarItems();
+        }
+    }
+}
